db9b5ee10130049b5d270deb59bf63adbd7f0f26
jmcb <joelsgp@protonmail.com>
2023-04-26 15:43:49 +0100

    Log to ignore dir

--- serial-monitor.sh
+++ serial-monitor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eux
-serial_log="serial-log.txt"
-serial_output="serial-output.txt"
+base_dir="ignore"
+serial_log="$base_dir/serial-log.txt"
+serial_output="$base_dir/serial-output.txt"
 arduino-cli monitor -p /dev/ttyACM0 --log-file "$serial_log" --log-level warn | tee -a "$serial_output"
